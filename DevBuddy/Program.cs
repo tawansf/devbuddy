38e35864cf0de96588e97a126ab0f72f558e29b1
Tawan Silva <tawan.sf.99@gmail.com>
2023-12-20 21:11:08 -0300

    app(#1): configuracao de ambiente e estrutura inicial

---
+++ DevBuddy/Program.cs
@@ -0,0 +1,29 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+await RunDevBuddy();
+
+async Task RunDevBuddy()
+{
+    try
+    {
+        var client = new DiscordSocketClient();
+        await client.LoginAsync(TokenType.Bot, Environment.GetEnvironmentVariable("TOKEN"));
+        await client.StartAsync();
+        client.Ready += async () =>
+        {
+            var guild = client.GetGuild(Convert.ToUInt64(Environment.GetEnvironmentVariable("GUILD")));
+            var channel = guild.GetTextChannel(Convert.ToUInt64(Environment.GetEnvironmentVariable("CHANNEL")));
+            await channel.CreateThreadAsync("Tópico de teste");
+            await client.DisposeAsync();
+        };
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex);
+        throw;
+    }
+}
+
+Console.WriteLine("Pressione qualquer tecla para parar a execução...");
+Console.ReadKey();
